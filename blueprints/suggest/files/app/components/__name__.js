14521daf2934a7dda742f8d5dcc9890296fc93b4
Piotr Pałek <piotrpalek@gmail.com>
2015-02-23 14:27:03 +0100

    move functionality from suggest-extra to suggest

--- blueprints/suggest/files/app/components/__name__.js
+++ blueprints/suggest/files/app/components/__name__.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
 import Suggest from 'suggest-addon/mixins/suggest';
-import SuggestExtra from 'suggest-addon/mixins/suggest-extra';
 
-export default Ember.Component.extend(Suggest, SuggestExtra,{
+export default Ember.Component.extend(Suggest,{
   didInsertElement: function(){
     // assgin your debounce method to 'doDebounce' e.g. this.set('doDebounce', this.searchCountry);
   }
